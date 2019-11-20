053b994275a021addb8eeb1f37030b864c7506fa
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2019-11-20 11:57:12 -0500

    dHMonthlyFractionFactors

--- drupal/dh_wsp/plugins/dh_variables/dHVwudsMpVars.class.php
+++ drupal/dh_wsp/plugins/dh_variables/dHVwudsMpVars.class.php
@@ -363,7 +363,7 @@ class dHMonthlyFractionFactors extends dHVarWithTableFieldBase {
     //$record = $result->fetchAssoc();
     //$record = $result->fetchAll(); //fetchAll() is closer...
     //$record = $result->fetchAllKeyed();
-    $record->fetchAllKeyed($result);
+    $record = $result->fetchAssoc();
     dpm($record,'record');
     return array_values($record); 
   }
