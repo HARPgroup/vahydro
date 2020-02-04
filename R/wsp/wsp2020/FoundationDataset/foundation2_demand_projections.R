f7f31512a7e04c523fb5b1470a95d0b803fb1baf
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-04 12:01:56 -0500

    Added wsp_ftype

--- R/wsp/wsp2020/FoundationDataset/foundation2_demand_projections.R
+++ R/wsp/wsp2020/FoundationDataset/foundation2_demand_projections.R
@@ -118,7 +118,17 @@ wsp2040 <- sqldf(
 wsp2020_2040 <- sqldf(
   "select a.*, a.mp_share as mp_2020_mgy, 
      b.fac_value as fac_value_2040, 
-     b.mp_share as mp_share_2040
+     b.mp_share as mp_share_2040,
+      CASE
+        WHEN a.facility_ftype in ('agriculture', 'irrigation') 
+          THEN 'wsp_plan_system-ssuag'
+        WHEN a.facility_ftype in ('manufacturing', 'nuclearpower', 'mining', 
+          'commercial', 'industrial', 'fossilpower', 'hydropower') 
+          THEN 'wsp_plan_system-ssulg'
+        WHEN a.facility_ftype in ('municipal') 
+          THEN 'wsp_plan_system-cws'
+          ELSE a.facility_ftype
+      END as wsp_ftype
   from wsp2020 as a 
   left outer join wsp2040 as b 
   on (
@@ -144,7 +154,17 @@ wsp_facility_2020_2040 <- sqldf(
   " select Facility_hydroid, facility_name, facility_ftype, fips_code, 
       avg(Latitude) as Latitude, avg(Longitude) as Longitude,
       sum(mp_2020_mgy) as fac_2020_mgy,
-      sum(mp_2040_mgy) as fac_2040_mgy
+      sum(mp_2040_mgy) as fac_2040_mgy,
+      CASE
+        WHEN facility_ftype in ('agriculture', 'irrigation') 
+          THEN 'wsp_plan_system-ssuag'
+        WHEN facility_ftype in ('manufacturing', 'nuclearpower', 'mining', 
+          'commercial', 'industrial', 'fossilpower', 'hydropower') 
+          THEN 'wsp_plan_system-ssulg'
+        WHEN facility_ftype in ('municipal') 
+          THEN 'wsp_plan_system-cws'
+          ELSE facility_ftype
+      END as wsp_ftype
     from wsp2020_2040 
     group by Facility_hydroid, facility_name, facility_ftype
   "
