83b52d18c5b5d22f581355524d8cb701530ce6f0
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-04 11:14:32 -0500

    Renamed columns

--- R/wsp/wsp2020/xlsx-to-LaTeX.R
+++ R/wsp/wsp2020/xlsx-to-LaTeX.R
@@ -8,8 +8,8 @@ source <- "wsp2020.fac.all.csv"
 folder <- "U:/OWS/foundation_datasets/wsp/wsp2020/"
 
 # Location of GIS_functions and gdb
-localpath <-"C:/Users/nrf46657/Desktop/VAHydro Development/GitHub/"
-source(paste(localpath,'hydro-tools/GIS_LAYERS','GIS_functions.R',sep='/'));
+localpath <-"C:/usr/local/home/git/"
+source('https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/GIS_LAYERS/GIS_functions.R');
 gdb_path <- "hydro-tools/GIS_LAYERS/HUC.gdb" #Location of HUC .gdb
 layer_name <- 'WBDHU6' #HUC6 layer withing the HUC .gdb
 
@@ -34,7 +34,8 @@ data_sp_cont <- sp_contain(paste(localpath,gdb_path,sep=""),layer_name,'all',dat
 data_sp_cont <- data.frame(data_sp_cont)
 ###########################################################################
 
-coverage_name <- "James"
+coverage_name <- "state"
+cov_label <- "HUC6"
 
 #Output all watershed options
 sqldf('SELECT DISTINCT Poly_Name
@@ -42,16 +43,21 @@ sqldf('SELECT DISTINCT Poly_Name
       ')
 
 #Select facilities within HUC of interest, Restict output to columns of interest
-sql <- paste('SELECT facility_name, 
-                      facility_ftype, 
-                      fac_2020_mgy, 
-                      fac_2040_mgy, 
-                      Poly_Name
-                  FROM data_sp_cont 
-                  WHERE Poly_Name = ','\"',coverage_name,'\"','
-                  ORDER BY fac_2020_mgy DESC
-                  LIMIT 5
-              ',sep="")
+sql <- '
+  SELECT facility_name, 
+    facility_ftype,
+    fac_2020_mgy, 
+    fac_2040_mgy, 
+    Poly_Name
+  FROM data_sp_cont
+  WHERE facility_ftype NOT LIKE \'wsp%\' '
+if (coverage_name != 'state') {
+  sql <- paste(sql, paste0('WHERE Poly_Name = ' , '\"', coverage_name,'\"') )
+}
+sql <- paste0(sql,
+  'ORDER BY fac_2020_mgy DESC
+                  LIMIT 5',
+  sep="")
 data <- sqldf(sql)
 
 
@@ -77,7 +83,7 @@ kable(data, "latex", booktabs = T,
                     "Facility Type",
                     "2020 (MGY)",
                     "2040 (MGY)",
-                    "HUC6")) %>%
+                    cov_label)) %>%
   kable_styling(latex_options = c("striped", "scale_down")) %>% 
   #column_spec(1, width = "5em") %>%
   #column_spec(2, width = "5em") %>%
