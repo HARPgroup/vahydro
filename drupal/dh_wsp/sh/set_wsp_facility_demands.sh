7b98cd2452124c0dd1ae2b0409c41da2bc5bbd6c
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-05 12:16:19 -0500

    Moved comments outside sql

--- drupal/dh_wsp/sh/set_wsp_facility_demands.sh
+++ drupal/dh_wsp/sh/set_wsp_facility_demands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$1" == "--help" ]; then
-  echo 1>&2 "Usage: set_use_fractions.sh [facility ydroid] "
+if [ $# -lt 1 ]; then
+  echo 1>&2 "Usage: set_wsp_facility_demands.sh hydroid (can be csv)"
   exit 2
 fi 
 
