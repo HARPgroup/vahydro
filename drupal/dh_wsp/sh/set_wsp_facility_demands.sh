e69933bbf12380523d43692a97336cd46556a82d
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-02-05 11:48:50 -0500

    Moved comments outside sql

--- drupal/dh_wsp/sh/set_wsp_facility_demands.sh
+++ drupal/dh_wsp/sh/set_wsp_facility_demands.sh
@@ -4,11 +4,11 @@ hydroids=$1
 cat modules/dh_wsp/sql/create_wsp_facility_demands.sql | psql -h dbase2 drupal.dh03 
 
 frac_query="
-select 'wsp2020_2020_mgy' as propname, 
-  'wsp_facility_mgy' as varkey,
+select 'dh_feature' as entity_type, 
   hydroid as featureid, 
+  'wsp_facility_mgy' as varkey,
+  'wsp2020_2020_mgy' as propname, 
   sum(fac_net_wd) as propvalue, 
-  'dh_feature' as entity_type, 
   'wsp_current_use_mgy' as propcode 
 from tmp_wsp_fac_net 
 where hydroid in ($hydroids)
@@ -23,8 +23,24 @@ n=`< /tmp/fhead.txt wc -l`
 nm="$((n - 4))"
 tail -n $nm /tmp/fhead.txt > /tmp/wsp_facility_current.txt 
 
+while IFS= read -r line; do
+  #echo "Text read from file: $line"
+  IFS="$IFS|" read entity_type featureid varkey propname propvalue <<< "$line"
+  drush scr modules/om/src/om_setprop.php cmd $entity_type $featureid $varkey $propname $propvalue
+done < /tmp/wsp_facility_current.txt 
+
 # NOW DO FUTURE
-frac_query=`cat modules/dh_wsp/sql/get_wsp_facility_future.sql`
+frac_query="
+select 'dh_feature' as entity_type, 
+  hydroid as featureid, 
+  'wsp_facility_mgy' as varkey,
+  'wsp2020_2020_mgy' as propname, 
+  sum(fac_net_wd) as propvalue, 
+  'wsp_future_use_mgy' as propcode 
+from tmp_wsp_fac_net 
+where hydroid in ($hydroids)
+group by hydroid;
+"
 echo $frac_query | PGOPTIONS='--client-min-messages=warning' psql -h dbase2 drupal.dh03 > /tmp/wsp_facility_current.txt 
 
 n=`< /tmp/wsp_facility_future.txt wc -l`
