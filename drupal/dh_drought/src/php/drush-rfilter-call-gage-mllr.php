d866e317285c52abeef98fdc2c3c7842c95724a1
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-04-03 15:23:27 -0400

    Batch in drush drought

--- drupal/dh_drought/src/php/drush-rfilter-call-gage-mllr.php
+++ drupal/dh_drought/src/php/drush-rfilter-call-gage-mllr.php
@@ -12,22 +12,33 @@ if (isset($a[1])) {
 } else {
   $year = date('Y');
 }
-$gage = entity_load_single('dh_feature', $fid);
-$gfield = field_get_items('dh_feature', $gage, 'dh_usgs_site_no');
-$gageid = array_shift($gfield);
-$gageid = $gageid['safe_value'];
-$now = date('U');
-$darg_hash = sha1("$gageid,$year");
-$rcode = "darg_hash <- '$darg_hash';\n";
-$rcode .= "[R]gage <- c($gageid);\n";
-$rcode .= "target_year <- '$year';\n";
-$rcode .= "fid <- $fid; \n";
-//$rcode .= file_get_contents ('/var/www/html/files/dh/mllr/analyze_winter.R');
-$rcode .= file_get_contents ('/var/www/html/d.dh/modules/dh_drought/src/r/analyze_winter.R');
-$rcode .= "[/R]\n";
+if ($fid == 'all') {
+  // query all gages
+  $q = db_query("select hydroid from dh_feature where bundle = 'usgsgage' LIMIT 3");
+  $fids = $result->fetchCol();
+} else {
+  // just grab the gage in question
+  $fids = array($fid);
+}
+foreach ($fids as $fid) {
+  error_log("Running MLLR for $year hydroid $fid ");
+  $gage = entity_load_single('dh_feature', $fid);
+  $gfield = field_get_items('dh_feature', $gage, 'dh_usgs_site_no');
+  $gageid = array_shift($gfield);
+  $gageid = $gageid['safe_value'];
+  $now = date('U');
+  $darg_hash = sha1("$gageid,$year");
+  $rcode = "darg_hash <- '$darg_hash';\n";
+  $rcode .= "[R]gage <- c($gageid);\n";
+  $rcode .= "target_year <- '$year';\n";
+  $rcode .= "fid <- $fid; \n";
+  //$rcode .= file_get_contents ('/var/www/html/files/dh/mllr/analyze_winter.R');
+  $rcode .= file_get_contents ('/var/www/html/d.dh/modules/dh_drought/src/r/analyze_winter.R');
+  $rcode .= "[/R]\n";
 
-//dpm($rcode, ' r code');;
-$rout = _filter_r_process($rcode, null, null);
-//dpm($rout, ' r output');;
+  //dpm($rcode, ' r code');;
+  $rout = _filter_r_process($rcode, null, null);
+  //dpm($rout, ' r output');;
+}
 
 ?>
