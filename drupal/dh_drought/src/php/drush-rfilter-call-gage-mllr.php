dacbd7e84ea425173d154f765e16c717fa4889e4
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-04-03 14:46:36 -0400

    skip MLLR generation if image file exists

---
+++ drupal/dh_drought/src/php/drush-rfilter-call-gage-mllr.php
@@ -0,0 +1,33 @@
+<?php
+module_load_include('module', 'dh');
+module_load_include('module', 'om');
+
+$a = array();
+while ($arg = drush_shift()) {
+  $args[] = $arg;
+}
+$fid = $a[1];
+if (isset($a[2])) {
+  $year = $a[2];
+} else {
+  $year = date('Y');
+}
+$gage = entity_load_single('dh_feature', $fid);
+$gfield = field_get_items('dh_feature', $gage, 'dh_usgs_site_no');
+$gageid = array_shift($gfield);
+$gageid = $gageid['safe_value'];
+$now = date('U');
+$darg_hash = sha1("$gageid,$year");
+$rcode = "darg_hash <- '$darg_hash';\n";
+$rcode .= "[R]gage <- c($gageid);\n";
+$rcode .= "target_year <- '$year';\n";
+$rcode .= "fid <- $fid; \n";
+//$rcode .= file_get_contents ('/var/www/html/files/dh/mllr/analyze_winter.R');
+$rcode .= file_get_contents ('/var/www/html/d.dh/modules/dh_drought/src/r/analyze_winter.R');
+$rcode .= "[/R]\n";
+
+//dpm($rcode, ' r code');;
+$rout = _filter_r_process($rcode, null, null);
+//dpm($rout, ' r output');;
+
+?>
