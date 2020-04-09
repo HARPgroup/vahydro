f801cb08d523ba5342a449de27c61ab83f33bdca
Joey Kleiner <jkleiner@vt.edu>
2020-04-09 16:06:30 -0400

    Update minor_basin_summary_maps.R

--- R/wsp/wsp2020/FoundationDataset/geo_summaries/minor_basin_summary_maps.R
+++ R/wsp/wsp2020/FoundationDataset/geo_summaries/minor_basin_summary_maps.R
@@ -191,7 +191,7 @@ map <- base_map +
     scale_fill_manual(values=c("darkolivegreen3","cornflowerblue","khaki2","plum3"), 
                     name = "Legend",
                     labels = c(">= 0%", 
-                               "-5% 50 0%", 
+                               "-5% to 0%", 
                                "-10% to -5%", 
                                "-20% to -10%"))+
   guides(fill = guide_legend(reverse=TRUE))+
