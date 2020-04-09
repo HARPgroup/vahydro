c4aef188acfb177ee5c1e1611e38639864f72e14
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2020-04-09 10:35:08 -0400

    statewide mapping script update

--- R/wsp/wsp2020/FoundationDataset/geo_summaries/statewide_summary_maps.R
+++ R/wsp/wsp2020/FoundationDataset/geo_summaries/statewide_summary_maps.R
@@ -15,7 +15,9 @@ STATES <- read.table(file = 'https://raw.githubusercontent.com/HARPgroup/cbp6/ma
 MinorBasins.csv <- read.table(file = 'https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/GIS_LAYERS/MinorBasins.csv', sep = ',', header = TRUE)
 RSeg.csv <- read.table(file = 'C:/Users/nrf46657/Desktop/VAHydro Development/GitHub/hydro-tools/GIS_LAYERS/VAHydro_RSegs.csv', sep = ',', header = TRUE)
 
+#Metric options include "7q10", "l30_Qout", "l90_Qout"
 metric <- "l30_Qout"
+plot_title <- "30 Day Low Flow (Percent Change 2020 to 2040)"
 
 folder <- "U:/OWS/foundation_datasets/wsp/wsp2020/"
 RSeg_summary <- read.csv(paste(folder,"metrics_watershed_",metric,".csv",sep=""))
@@ -86,8 +88,6 @@ MB@data <- MB@data[,-c(2:3)]
 MB.df <- fortify(MB, region = 'id')
 MB.df <- merge(MB.df, MB@data, by = 'id')
 
-
-
 ######################################################################################################
 ### PROCESS RSegs
 ######################################################################################################
@@ -111,9 +111,42 @@ RSeg_data <- sqldf(RSeg_valid_geoms)
 length(RSeg_data[,1])
 
 ######################################################################################################
-RSeg_data <- st_as_sf(RSeg_data, wkt = 'geom')
+######################################################################################################
+#colnames(RSeg_data)
+group_0_plus <- paste("SELECT *
+                  FROM RSeg_data
+                  WHERE chg_11_to_13 >= 0")  
+group_0_plus <- sqldf(group_0_plus)
+group_0_plus <- st_as_sf(group_0_plus, wkt = 'geom')
+#-----------------------------------------------------------------------------------------------------
+group_neg5_0 <- paste("SELECT *
+                  FROM RSeg_data
+                  WHERE chg_11_to_13 < 0 AND chg_11_to_13 >= -5")  
+group_neg5_0 <- sqldf(group_neg5_0)
+group_neg5_0 <- st_as_sf(group_neg5_0, wkt = 'geom')
+#-----------------------------------------------------------------------------------------------------
+group_neg10_neg5 <- paste("SELECT *
+                  FROM RSeg_data
+                  WHERE chg_11_to_13 < -5 AND chg_11_to_13 >= -10")  
+group_neg10_neg5 <- sqldf(group_neg10_neg5)
+group_neg10_neg5 <- st_as_sf(group_neg10_neg5, wkt = 'geom')
+#-----------------------------------------------------------------------------------------------------
+group_neg20_neg10 <- paste("SELECT *
+                  FROM RSeg_data
+                  WHERE chg_11_to_13 < -10 AND chg_11_to_13 >= -20")  
+group_neg20_neg10 <- sqldf(group_neg20_neg10)
+group_neg20_neg10 <- st_as_sf(group_neg20_neg10, wkt = 'geom')
+#-----------------------------------------------------------------------------------------------------
+group_negInf_neg20 <- paste("SELECT *
+                  FROM RSeg_data
+                  WHERE chg_11_to_13 <= -20")  
+group_negInf_neg20 <- sqldf(group_negInf_neg20)
+group_negInf_neg20 <- st_as_sf(group_negInf_neg20, wkt = 'geom')
 ######################################################################################################
 
+######################################################################################################
+RSeg_data <- st_as_sf(RSeg_data, wkt = 'geom')
+######################################################################################################
 
 ######################################################################################################
 ### GENERATE YOUR MAP  ###############################################################################
@@ -126,26 +159,52 @@ base_map  <- ggplot(data = state.df, aes(x = long, y = lat, group = group))+
 #colnames(RSeg_data)
 map <- base_map + 
   #no group on this layer, so don't inherit aes
-  geom_sf(data = RSeg_data,aes(geometry = geom,fill = chg_11_to_13), inherit.aes = FALSE)+ 
+  geom_sf(data = RSeg_data,aes(geometry = geom,fill = 'aliceblue'), inherit.aes = FALSE,  show.legend=FALSE)+ 
+  
+  geom_sf(data = group_0_plus,aes(geometry = geom,fill = 'antiquewhite'), inherit.aes = FALSE)+ 
+  geom_sf(data = group_neg5_0,aes(geometry = geom,fill = 'antiquewhite1'), inherit.aes = FALSE)+ 
+  geom_sf(data = group_neg10_neg5,aes(geometry = geom,fill = 'antiquewhite2'), inherit.aes = FALSE)+ 
+  geom_sf(data = group_neg20_neg10,aes(geometry = geom,fill = 'antiquewhite3'), inherit.aes = FALSE)+ 
+  geom_sf(data = group_negInf_neg20,aes(geometry = geom,fill = 'antiquewhite4'), inherit.aes = FALSE)+ 
+
+    scale_fill_manual(values=c("gray55","darkolivegreen3","cornflowerblue","khaki2","plum3","coral3"), 
+                    name = "Legend",
+                    labels = c("In Progress",
+                               ">= 0%", 
+                               "-5% 50 0%", 
+                               "-10% to -5%", 
+                               "-20% to -10%", 
+                               "More than -20%"))+
+  guides(fill = guide_legend(reverse=TRUE))+
   geom_polygon(data = MB.df, color="black", fill = NA,lwd=0.5)+
-  ggtitle("30 Day Low Flow (2020 vs 2040)")+
-  #guides(fill=guide_colorbar(title="Legend\nPercent Change")) +
-  guides(fill=guide_colorbar(title="Percent Change")) +
-  # scale_fill_gradient2(low = 'white',high = 'blue',
-  #                       labels=function(x) format(x, big.mark = ",", scientific = FALSE),
-  #                       trans = "log10") +
+  ggtitle(plot_title)+
   theme(legend.justification=c(0,1), legend.position=c(0,1)) +
   xlab('Longitude (deg W)') + ylab('Latitude (deg N)')+
-  north(bbDF, location = 'topright', symbol = 12, scale=0.1)+
+  north(bbDF, symbol = 3, scale=0.12,
+          anchor = c(
+            x = extent$x[2],
+            y = extent$y[2]-(0.002*extent$y[2])
+          )
+        )+
   scalebar(bbDF, location = 'bottomleft', dist = 100, dist_unit = 'mi', 
            transform = TRUE, model = 'WGS84',st.bottom=FALSE, 
            st.size = 3.5, st.dist = 0.0285,
            anchor = c(
              x = (((extent$x[2] - extent$x[1])/2)+extent$x[1])-2.0,
              y = extent$y[1]+(extent$y[1])*0.001
-           ))
+           ))+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank())
 
-ggsave(plot = map, file = paste0(folder, "state_plan_figures/statewide/",metric,"_map.png"), width=6.5, height=5)
+ggsave(plot = map, file = paste0(folder, "state_plan_figures/statewide/chg_11_to_13_",metric,"_map.png"), width=6.5, height=5)
 
 
 
