117f4a167527285d38afb8d75c6bc5895ac2a044
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2020-04-27 18:09:31 -0400

    restore element-runfile.R again?

--- R/wsp/wsp2020/impounded_subwatershed_summary.R
+++ R/wsp/wsp2020/impounded_subwatershed_summary.R
@@ -9,8 +9,8 @@ source(paste(basepath,'config.R',sep='/'))
 
 # Camp Creek - 279187, South Anna - 207771, James River - 214907, Rapp above Hazel confluence 257471
 # Rapidan above Rapp - 258123
-elid = 352004 
-runid = 14
+elid = 352006 
+runid = 13
 
 fname = 'watershed'
 omsite = "http://deq2.bse.vt.edu"
