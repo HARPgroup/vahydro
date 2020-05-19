d16f9ce1fc285f3ee93af4c445b505260828ec92
Mineva <Janet.Mineva@deq.virginia.gov>
2020-05-19 16:20:42 -0400

    select distinct from exempt users list

--- R/wsp/wsp2020/FoundationDataset/foundation2_demand_projections.R
+++ R/wsp/wsp2020/FoundationDataset/foundation2_demand_projections.R
@@ -1,5 +1,5 @@
-require(sqldf)
-require(httr)
+library(sqldf)
+library(httr)
 
 #----------------------------------------------
 # USER INPUTS
@@ -18,7 +18,10 @@ options(scipen = 20)
 
 ################# Load Exempt Users export ##################################################
 exempt <- read.csv("U:/OWS/foundation_datasets/wsp/wsp2020/ows-exemptions-export.csv")
-
+#exempt users screen shows permit linkage - meaning if a Facility is linked to multiple permits, a record for each permit will show - select distinct ensures no duplicates
+exempt <- sqldf("SELECT distinct mp_hydroid, final_exempt_propcode, final_exempt_propvalue_mgd
+                FROM exempt
+                ")
 ################# wd_current_mgy Facilities Data Summary ####################################
 
 localpath <- tempdir()
@@ -206,7 +209,7 @@ wsp2020_2040 <- sqldf("SELECT MP_hydroid,
              WHERE facility_status != 'unknown'")
 
 #append exempt values 
-wsp2020_2040 <- sqldf('SELECT a.*, b.final_exempt_propcode, b.final_exempt_propvalue_mgd
+wsp2020_20402 <- sqldf('SELECT a.*, b.final_exempt_propcode, b.final_exempt_propvalue_mgd
                       FROM wsp2020_2040 a
                       LEFT OUTER JOIN exempt b
                       ON a.MP_hydroid = b.mp_hydroid
