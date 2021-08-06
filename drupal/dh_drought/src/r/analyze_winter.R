2989870ac967dd53b876b0ba9e0e5c28ddef0506
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-08-06 15:45:28 -0400

    rows! not cols

--- drupal/dh_drought/src/r/analyze_winter.R
+++ drupal/dh_drought/src/r/analyze_winter.R
@@ -46,7 +46,7 @@ gages$staid <- as.character(gages$staid)
 # override the file save directory
 file_directory = '/var/www/html/images/dh';
 message(paste("Generating MLLR images for a set of", length(gages), "stream gages"))
-for (i in 1:length(gages)) {
+for (i in 1:nrow(gages)) {
   gage_info <- gages[i,]
   gage_id = gage_info$staid
   fid <- gage_info$hydroid
