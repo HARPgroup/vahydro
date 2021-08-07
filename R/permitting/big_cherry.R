fa07296cdd3618c10407abb9cbffcb8cfb2c092f
Robert W. Burgholzer <rburghol@vt.edu>
2021-08-07 13:20:11 -0400

    stash

--- R/permitting/big_cherry.R
+++ R/permitting/big_cherry.R
@@ -1,16 +1,18 @@
 library('hydrotools')
 library('zoo')
+basepath='/var/www/R';
+source('/var/www/R/config.R');
 
-datbc201 <- om_get_rundata(352078, 201)
-datbc301 <- om_get_rundata(352078, 301)
-datbc401 <- om_get_rundata(352078, 401)
-datbc601 <- om_get_rundata(352078, 601)
-datbc6011 <- om_get_rundata(352078, 6011)
+datbc201 <- om_get_rundata(352078, 201, site = omsite)
+datbc301 <- om_get_rundata(352078, 301, site = omsite)
+datbc401 <- om_get_rundata(352078, 401, site = omsite)
+datbc601 <- om_get_rundata(352078, 601, site = omsite)
+datbc6011 <- om_get_rundata(352078, 6011, site = omsite)
 
-datbcfac201 <- om_get_rundata(247415, 201)
-datbcfac301 <- om_get_rundata(247415, 301)
-datbcfac401 <- om_get_rundata(247415, 401)
-datbcfac6011 <- om_get_rundata(247415, 6011)
+datbcfac201 <- om_get_rundata(247415, 201, site = omsite)
+datbcfac301 <- om_get_rundata(247415, 301, site = omsite)
+datbcfac401 <- om_get_rundata(247415, 401, site = omsite)
+datbcfac6011 <- om_get_rundata(247415, 6011, site = omsite)
  
 
 datbc[200:250,c(wd_channel_cfs', 'Qlocal_channel', 'bc_release_cfs', 'impoundment_Qout')]
