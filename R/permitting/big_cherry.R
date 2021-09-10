561358605cb00fbc1b6112f3e6e43baddb8d1014
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-09-10 11:04:20 -0400

    add base demand to summary vars for systems

--- R/permitting/big_cherry.R
+++ R/permitting/big_cherry.R
@@ -18,8 +18,16 @@ datbc301 <- om_get_rundata(352078, 301, site = omsite)
 datbc4011 <- om_get_rundata(352078, 4011, site = omsite)
 datbc401 <- om_get_rundata(352078, 401, site = omsite)
 datbc601 <- om_get_rundata(352078, 601, site = omsite)
+datbc602 <- om_get_rundata(352078, 602, site = omsite)
 datbc6011 <- om_get_rundata(352078, 6011, site = omsite)
 datbc6014 <- om_get_rundata(352078, 6014, site = omsite)
+bccc <- as.data.frame(
+  datbc602[,
+    c("impoundment_use_remain_mg",
+      "impoundment_days_remaining",
+      "bc_release_cfs")
+  ]
+)
 
 datbcfac4011 <- om_get_rundata(247415, 4011, site = omsite)
 datbcfac201 <- om_get_rundata(247415, 201, site = omsite)
@@ -27,18 +35,37 @@ datbcfac301 <- om_get_rundata(247415, 301, site = omsite)
 datbcfac401 <- om_get_rundata(247415, 401, site = omsite)
 datbcfac6011 <- om_get_rundata(247415, 6011, site = omsite)
 datbcfac601 <- om_get_rundata(247415, 601, site = omsite)
-datbcfac602 <- om_get_rundata(247415, 602, site = omsite)
 datbcfac6014 <- om_get_rundata(247415, 6014, site = omsite)
+quantile(datbcfac602$available_mgd,probs=c(0,0.01,0.05,0.10, 0.25,0.5))
 
-datbcfac602[,c("Qintake", "bc_release_cfs", "ps_local_mgd", "Qnatural", "wd_mgd")][190:205]
 
+dev.off()
 hydroTSM::fdc(cbind(datbcfac4011$Qnatural, datbcfac4011$Qintake))
 hydroTSM::fdc(cbind(datbcfac601$Qnatural, datbcfac601$Qintake))
-hydroTSM::fdc(cbind(datbcfac602$Qnatural, datbcfac602$Qintake, datbcfac602$flowby, datbcfac602$flowby_pof))
-cccc <- as.data.frame(cbind(datbcfac602$Qnatural, datbcfac602$Qintake, datbcfac602$flowby, datbcfac602$flowby_pof))
+hydroTSM::fdc(cbind(datbcfac602$Qnatural, datbcfac602$Qintake))
+cccc <- as.data.frame(
+  datbcfac602[,c(
+    "Qnatural",
+    "discharge_mgd",
+    "flowby_pof",
+    "flowby",
+    "bc_release_cfs",
+    "Qintake",
+    "available_mgd",
+    "wd_mgd",
+    "unmet_demand_mgd",
+    "reservoir_use_remain_mg")]
+  )
 hydroTSM::fdc(cccc)
-ro6014 <- om_get_rundata(247387,  6014, site = omsite)
-ro601 <- om_get_rundata(247387,  601, site = omsite)
+# ro container = 247387
+# ro cbp5 = 347582
+ro6014 <- om_get_rundata(347582,  6014, site = omsite)
+ro601 <- om_get_rundata(347582,  601, site = omsite)
+ro602 <- om_get_rundata(347582,  602, site = omsite)
+ro6012 <- om_get_rundata(347582,  6012, site = omsite)
+ro6013 <- om_get_rundata(347582,  6013, site = omsite)
+quantile(ro6012$Qunit)
+quantile(ro6013$Qunit)
 hydroTSM::fdc(datbcfac602$flowby)
 247387
 
@@ -75,3 +102,9 @@ elfgen_huc(runid, hydroid, huc_level, dataset, scenprop, ds, save_directory, sav
 
 
 fdc(cbind(datbcfac401$Qnatural, datbcfac401$Qintake))
+
+datbcfac602 <- om_get_rundata(247415, 602, site = omsite)
+datbcfac6012 <- om_get_rundata(247415, 6012, site = omsite)
+datbcfac6013 <- om_get_rundata(247415, 6013, site = omsite)
+om_ts_diff(datbcfac6012, datbcfac6013, "reservoir_use_remain_mg", "reservoir_use_remain_mg")
+
