7c158b44f7da116ad78e64c2824474f9c141e6dc
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2021-09-15 10:55:40 -0400

    added better fduration labels

--- R/permitting/big_cherry.R
+++ R/permitting/big_cherry.R
@@ -34,6 +34,7 @@ datbcfac201 <- om_get_rundata(247415, 201, site = omsite)
 datbcfac301 <- om_get_rundata(247415, 301, site = omsite)
 datbcfac401 <- om_get_rundata(247415, 401, site = omsite)
 datbcfac6011 <- om_get_rundata(247415, 6011, site = omsite)
+datbcfac6012 <- om_get_rundata(247415, 6012, site = omsite)
 datbcfac601 <- om_get_rundata(247415, 601, site = omsite)
 datbcfac6014 <- om_get_rundata(247415, 6014, site = omsite)
 quantile(datbcfac602$available_mgd,probs=c(0,0.01,0.05,0.10, 0.25,0.5))
@@ -44,7 +45,15 @@ hydroTSM::fdc(
   cbind(datbcfac6011$Qnatural, datbcfac6011$Qintake),
   yat = c(1,5,10,25,100,400)
 )
-axis(2,at = c(1,5,10,25,100,400))
+hydroTSM::fdc(
+  cbind(datbcfac4011$Qnatural, datbcfac4011$Qintake),
+  yat = c(1,5,10,25,100,400)
+)
+hydroTSM::fdc(
+  cbind(datbcfac6012$Qnatural, datbcfac6012$Qintake),
+  yat = c(1,5,10,25,100,400)
+)
+
 hydroTSM::fdc(cbind(datbcfac4011$Qnatural, datbcfac4011$Qintake))
 hydroTSM::fdc(cbind(datbcfac601$Qnatural, datbcfac601$Qintake))
 hydroTSM::fdc(cbind(datbcfac6013$Qnatural, datbcfac6013$Qintake))
