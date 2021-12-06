26423a22dbedddd35da71a84929df589e2e3b0a0
Burgholzer <robert.burgholzer@deq.virginia.gov>
2021-12-06 15:46:09 -0500

    message

--- drupal/dh_drought/src/r/analyze_winter.R
+++ drupal/dh_drought/src/r/analyze_winter.R
@@ -71,6 +71,8 @@ for (i in 1:nrow(gages)) {
     # check in case the file is out of date, in which case overwrite it anyhow
     linfo = file.info(filepath)
     if (as.Date(linfo$mtime) < as.Date(paste0(target_year, '-03-01'))) {
+      message(paste( gage_id, "is up to date as of ",as.Date(linfo$mtime)  ))
+      message("*********** SKIPPING ************* ")
       next
     }
   }
