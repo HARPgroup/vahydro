3286c7691f772e1d4d3203e5335746d49d1ef632
Burgholzer <robert.burgholzer@deq.virginia.gov>
2021-12-08 12:46:59 -0500

    incorporate changes to narrative and layout

--- R/wsp/wsp2020/cia_jrcc.R
+++ R/wsp/wsp2020/cia_jrcc.R
@@ -4,16 +4,17 @@
 library("sqldf")
 library("stringr") #for str_remove()
 library("hydrotools")
+library("openmi.om")
 
 # Load Libraries
 basepath='/var/www/R';
 site <- "http://deq2.bse.vt.edu/d.dh"    #Specify the site of interest, either d.bet OR d.dh
-source("/var/www/R/config.local.private"); 
+source("/var/www/R/config.local.private");
 source(paste(basepath,'config.R',sep='/'))
 
 # get the DA, need to grab a model output first in order to ensure segments with a channel subcomp
 # are included
-# 
+#
 
 # GET DA
 df <- data.frame(
@@ -22,7 +23,10 @@ df <- data.frame(
   'runlabel' = c('QBaseline_2020', 'comp_da', 'subcomp_da'),
   'metric' = c('Qbaseline', 'drainage_area', 'drainage_area')
 )
-da_data <- om_vahydro_metric_grid(metric, df)
+da_data <- om_vahydro_metric_grid(
+  metric = metric, runids = df,
+  base_url = paste(site,'entity-model-prop-level-export',sep="/")
+)
 da_data <- sqldf(
   "select pid, comp_da, subcomp_da,
    CASE
@@ -35,23 +39,61 @@ da_data <- sqldf(
 # GET RIVERSEG l90_Qout DATA
 df <- data.frame(
   'model_version' = c('vahydro-1.0',  'vahydro-1.0',  'vahydro-1.0'),
-  'runid' = c('runid_11', 'runid_12', 'runid_13'),
+  'runid' = c('runid_11', 'runid_400', 'runid_600'),
   'metric' = c('l90_Qout', 'l90_Qout','l90_Qout'),
-  'runlabel' = c('L90_2020', 'L90_2030', 'L90_2040')
+  'runlabel' = c('L90_2020', 'L90_perm', 'L90_prop')
 )
-wshed_data <- om_vahydro_metric_grid(metric, df)
+wshed_data <- om_vahydro_metric_grid(
+  metric = metric, runids = df,
+  base_url = paste(site,'entity-model-prop-level-export',sep="/")
+)
 
 #JOIN DA, AND RESTRICT TO OD ONLY
 wshed_data <- sqldf(
-  "select a.*, b.da 
-   from wshed_data as a 
-  left outer join da_data as b 
+  "select a.*, b.da
+   from wshed_data as a
+  left outer join da_data as b
   on (a.pid = b.pid)
-  where hydrocode like 'vahydrosw_wshed_OD%'
+  where hydrocode like 'vahydrosw_wshed_J%'
   order by da
   ")
 # write.csv(wshed_data,paste(export_path,'tables_maps/Xfigures/','wshed_data.csv',sep=""))
 #--------------------------------------------------
+
+riv_data = sqldf("select * from wshed_data where propname like '%rivanna%'")
+
+# debug the rivanna
+# River channel above confluence
+# Reports freaky accumulative Qup
+rd <- om_get_rundata(215029, 400, site = omsite)
+# Next up is
+riva <- om_get_rundata
+quantile(riva$Qout)
+# Tribs to South Rivanna
+rt_bc <- om_get_rundata(351963 , 400, site = omsite)
+quantile(rt_bc$Qout)
+rt_bm <- om_get_rundata(337728 , 400, site = omsite)
+quantile(rt_bm$Qout)
+rt_ic <- om_get_rundata(337852 , 400, site = omsite)
+quantile(rt_ic$Qout)
+rt_me <- om_get_rundata(337722 , 400, site = omsite)
+quantile(rt_me$Qout)
+# Ivy Creek runoff
+ro_ic <- om_get_rundata(352034  , 400, site = omsite)
+quantile(ro_ic$Runit)
+
+source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/VAHydro-2.0/find_name.R")
+
+ds <- RomDataSource$new(site, 'restws_admin')
+ds$get_token(rest_pw)
+model_prop <- RomProperty$new(ds,list(featureid = 68183, entity_type = 'dh_feature', propcode = 'vahydro-1.0'), TRUE)
+src_json_node <- paste(site, "node/62", model_prop$pid, sep="/")
+load_txt <- ds$auth_read(src_json_node, "text/json", "")
+load_objects <- fromJSON(load_txt)
+model_json <- load_objects[[model_prop$propname]]
+model <-  openmi_om_load(model_json)
+
+
 
 # GET RIVERSEG DATA
 df.props <- data.frame(
