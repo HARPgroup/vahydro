1a3ad2dc16a26a5acb6eb92a398a0a1815f323bc
Joey Kleiner <jkleiner@vt.edu>
2022-02-22 12:13:02 -0500

    Update salem.R

--- R/permitting/Salem WTP/salem.R
+++ R/permitting/Salem WTP/salem.R
@@ -3,92 +3,41 @@ library('zoo')
 basepath='/var/www/R';
 source("/var/www/R/config.R")
 
-# river
-rpid = 4713208
-relid = 249169 
+################################################################################################
+# LOAD MODEL IDs:
+rseg_om_id <- 249169
+fac_om_id <- 306768
+################################################################################################
+################################################################################################
 
-datr401 <- om_get_rundata(relid, 401, site = omsite)
-datr601 <- om_get_rundata(relid, 601, site = omsite)
-bccc <- as.data.frame(
-  datbc602[,
-    c("impoundment_use_remain_mg",
-      "impoundment_days_remaining",
-      "bc_release_cfs")
-  ]
-)
+#runid <- 11
+#runid <- 2011
+#runid <- 4011
+runid <- 6011
 
-# facility
-fpid = 4827216
-felid = 306768 
+facdat <- om_get_rundata(fac_om_id, runid, site = omsite)
+rsegdat <- om_get_rundata(rseg_om_id, runid, site = omsite)
 
-datf11 <- om_get_rundata(felid, 11, site = omsite)
-datf13 <- om_get_rundata(felid, 13, site = omsite)
-datf401 <- om_get_rundata(felid, 401, site = omsite)
-datf601 <- om_get_rundata(felid, 601, site = omsite)
-quantile(datf601$Qreach,probs=c(0,0.01,0.05,0.10, 0.25,0.5))
-quantile(datf601$Qintake,probs=c(0,0.01,0.05,0.10, 0.25,0.5))
+facdat_df <- data.frame(facdat)
+rsegdat_df <- data.frame(rsegdat)
+################################################################################################
+# FACILITY
+# fac_qa <- sqldf("select year,month,day, wd_mgd, discharge_mgd, consumption
+#                  from 'facdat_df' WHERE year = 1999 AND month = 1")
+fac_qa_2011 <- sqldf("select year,month,day, wd_mgd, discharge_mgd, consumption, 
+                 current_mgd,
+                 gw_demand_mgd
+                 from 'facdat_df'")
 
+colnames(facdat_df)
+quantile(facdat_df$gw_demand_mgd, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
 
-dev.off()
-hydroTSM::fdc(
-  cbind(datf6011$Qnatural, datf6001$Qintake),
-  yat = c(1,5,10,25,100,400)
-)
-hydroTSM::fdc(
-  cbind(datf6011$Qnatural, datf6011$Qintake),
-  yat = c(1,5,10,25,100,400)
-)
-hydroTSM::fdc(
-  cbind(datf600$Qnatural, datf600$Qintake),
-  yat = c(1,5,10,25,100,400)
-)
-hydroTSM::fdc(
-  cbind(datf6014$Qnatural, datf6014$Qintake),
-  yat = c(1,5,10,25,100,400)
-)
+facdat_df$gw_sw_factor
+facdat_df$unmet_demand_mgd
+quantile(facdat_df$unmet_demand_mgd, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
 
+################################################################################################
+# RIVER SEGMENT
 
-[runid_600:wd_cumulative_mgd]
-
-hydroTSM::fdc(cbind(datf4011$Qnatural, datf4011$Qintake))
-hydroTSM::fdc(cbind(datf601$Qnatural, datf601$Qintake))
-hydroTSM::fdc(cbind(datf6013$Qnatural, datf6013$Qintake))
-hydroTSM::fdc(cbind(datf6014$Qnatural, datf6014$Qintake))
-cccc <- as.data.frame(
-  datf6013[,c(
-    "Qnatural",
-    "discharge_mgd",
-    "flowby_pof",
-    "flowby",
-    "bc_release_cfs",
-    "Qintake",
-    "available_mgd",
-    "wd_mgd",
-    "unmet_demand_mgd",
-    "reservoir_use_remain_mg")]
-  )
-hydroTSM::fdc(cccc)
-datf6013[300:370,c('Qnatural', 'reservoir_use_remain_mg', 'bc_release_cfs')]
-
-sample <- rbind(as.data.frame(datf600[5813,]),as.data.frame(datf6011[700,]))
-xcsam <- as.data.frame(
-  sample[,c(
-    "Qnatural",
-    "discharge_mgd",
-    "flowby_pof",
-    "flowby",
-    "bc_release_cfs",
-    "Qintake",
-    "available_mgd",
-    "wd_mgd",
-    "unmet_demand_mgd",
-    "reservoir_use_remain_mg")]
-)
-
-fdc(cbind(datf401$Qnatural, datf401$Qintake))
-
-datf602 <- om_get_rundata(felid, 602, site = omsite)
-datf6012 <- om_get_rundata(felid, 6012, site = omsite)
-datf6013 <- om_get_rundata(felid, 6013, site = omsite)
-om_ts_diff(datf6012, datf6013, "reservoir_use_remain_mg", "reservoir_use_remain_mg")
-
+colnames(rsegdat_df)
+quantile(rsegdat_df$gw_demand_mgd, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
