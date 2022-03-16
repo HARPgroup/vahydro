58d95043d1aa0c31f53597d953bc2ea89816c983
Joey Kleiner <jkleiner@vt.edu>
2022-03-16 15:34:36 -0400

    Update salem.R
    - Calculate final unmet demand if using gw to offset unmet demands from sw

--- R/permitting/Salem WTP/salem.R
+++ R/permitting/Salem WTP/salem.R
@@ -37,13 +37,56 @@ facdat_df$gw_sw_factor
 facdat_df$unmet_demand_mgd
 quantile(facdat_df$unmet_demand_mgd, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
 
+
+quantile(facdat_df$Qnatural, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
+
+################################################################################################
+colnames(facdat_df)
+fac_qa_6011 <- sqldf("select year,month,day,base_demand_mgd,
+                                             wd_mgd,
+                                             unmet_demand_mgd,
+                                             Qintake/1.547 AS Qintake_mgd,
+                                             flowby/1.547 AS flowby_mgd,
+                                             available_mgd
+                 from facdat_df")
+
+
+################################################################################################
+################################################################################################
+# GW
+fac_qa_6011 <- sqldf("select year,month,day,base_demand_mgd,
+                                             wd_mgd,
+                                             unmet_demand_mgd,
+                                             Qintake/1.547 AS Qintake_mgd,
+                                             flowby/1.547 AS flowby_mgd,
+                                             available_mgd,
+                                             gw_demand_mgd,
+                                             2.6 AS gw_capacity_mgd,
+                                             2.6 - gw_demand_mgd AS gw_surplus_mgd,
+                                          CASE WHEN (unmet_demand_mgd - (2.6 - gw_demand_mgd) < 1) THEN 0
+                		                        ELSE unmet_demand_mgd - (2.6 - gw_demand_mgd)
+                                          END AS final_unmet_demand_mgd
+                     from facdat_df")
+
+Unmet_Demand_MGY <- (sum(fac_qa_6011$unmet_demand_mgd)/length(fac_qa_6011$unmet_demand_mgd))*365
+Final_Unmet_Demand_MGY <- (sum(fac_qa_6011$final_unmet_demand_mgd)/length(fac_qa_6011$final_unmet_demand_mgd))*365
+
+unmet_days <- length(sqldf("SELECT * FROM fac_qa_6011 WHERE unmet_demand_mgd > 0")$unmet_demand_mgd)
+final_unmet_days <- length(sqldf("SELECT * FROM fac_qa_6011 WHERE final_unmet_demand_mgd > 0")$final_unmet_demand_mgd)
 ################################################################################################
 # RIVER SEGMENT
 
 colnames(rsegdat_df)
 quantile(rsegdat_df$gw_demand_mgd, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
 
+quantile(rsegdat_df$Runit*380, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
+quantile(rsegdat_df$Qout, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
+quantile(rsegdat_df$Qup, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
 
+quantile(rsegdat_df$Qnatural, probs=c(0,0.1,0.25,0.5,0.75,0.9,1.0))
+
+################################################################################################
+#-----------------------------------------------------------------------
 
 # calibration comparison
 # Wayside Park river segment & Glenvar Gage
