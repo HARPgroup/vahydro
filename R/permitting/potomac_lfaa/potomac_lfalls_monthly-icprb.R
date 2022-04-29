adbc01046bb162b69ee28328675da15be23889f6
Burgholzer <robert.burgholzer@deq.virginia.gov>
2022-04-29 12:27:27 -0400

    cleanup,functionalize

--- R/permitting/potomac_lfaa/potomac_lfalls_monthly-icprb.R
+++ R/permitting/potomac_lfaa/potomac_lfalls_monthly-icprb.R
@@ -11,116 +11,12 @@ source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/VAHydro-2
 source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/VAHydro-2.0/find_name.R") #Used during development
 source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/R/fac_utils.R") #Used until fac_utils is packaged
 
-# Load the gage flow record
-gageid = '01638500'
-historic <- dataRetrieval::readNWISdv(gageid,'00060')
-historic$month <- month(historic$Date)
-historic$year <- year(historic$Date)
-gage_sum_historic <- om_flow_table(historic, "X_00060_00003")
-
-# Load the IFIM feature and data
-# to do: put this on github as json data
+# Load the monthly flow record
 source("c:/usr/local/home/git/vahydro/R/permitting/potomac_lfaa/ifim_data_lfalls.R")
 
 # load PoR time series from Gage and ICPRB
 # compare PoR gage time series with
 icprb_monthly_lf <- read.csv("https://raw.githubusercontent.com/HARPgroup/vahydro/master/data/lfalls_nat_monthly_data.csv")
-icprb_monthly_prod <- read.csv("https://raw.githubusercontent.com/HARPgroup/vahydro/master/data/wma_production.csv")
-icprb_monthly_prod$month <- month(as.Date(icprb_monthly_prod$thisdate,format="%m/%d/%Y"))
-icprb_monthly_prod$year <- year(as.Date(icprb_monthly_prod$thisdate,format="%m/%d/%Y"))
-
-# monthly mean flows from ICPRB
-da_por <- 9651.0 # https://waterdata.usgs.gov/nwis/uv?site_no=01638500
-da_lf <- 11586.6 # d.dh/admin/content/dh_features/manage/68363/dh_properties
-
-nat_lf <- historic[c("Date", "X_00060_00003", "year", "month")]
-colnames(nat_lf) <- c('Date', 'Flow', "year", "month")
-nat_lf$Flow <- (da_lf / da_por) * nat_lf$Flow
-
-icprb_prod_max <- sqldf(
-  "
-   select month,
-     max(wssc_pot) as wssc_pot,
-     max(wa_gf) as  wa_gf,
-     max(wa_lf) as wa_lf,
-     max(fw_pot) as fw_pot,
-     max(rville) as rville,
-     max(up_cu) as up_cu
-   from icprb_monthly_prod where year >= 2015
-   group by month
-  "
-)
-
-alt_lf <- sqldf(
-  "
-   select a.cyear as year, a.month,
-    (b.wssc_pot + b.wa_gf + b.wa_lf
-      + b.fw_pot + b.rville) as demand_mgd,
-      a.lfalls_nat as Flow
-   from icprb_monthly_lf as a
-   left outer join icprb_prod_max as b
-   on (
-     a.month = b.month
-   )
-  "
-)
-alt_lf$Date <- paste0(alt_lf$year,'-',alt_lf$month,'-01')
-
-
-alt_lf <- sqldf(
-  "
-    select *,
-      (Flow * 0.2)/1.547 as avail_p20_mgd,
-      CASE
-        WHEN Flow >= (100.0 * 1.547)
-          THEN (Flow - (100.0 * 1.547))/1.547
-        ELSE 0.0
-      END as avail_curr_mgd,
-      CASE
-        WHEN Flow >= (500.0 * 1.547)
-          THEN (Flow - (500.0 * 1.547))/1.547
-        ELSE 0.0
-      END as avail_q500_mgd
-    from alt_lf
-  "
-)
-
-# add in the demands, factoring for flowby
-alt_lf <- sqldf(
-  "
-    select a.*,
-      CASE
-        WHEN demand_mgd > avail_curr_mgd THEN avail_curr_mgd
-        ELSE demand_mgd
-      END as wd_curr_mgd,
-      CASE
-        WHEN demand_mgd > avail_p20_mgd THEN avail_p20_mgd
-        ELSE demand_mgd
-      END as wd_p20_mgd,
-      CASE
-        WHEN demand_mgd > avail_q500_mgd THEN avail_q500_mgd
-        ELSE demand_mgd
-      END as wd_q500_mgd
-    from alt_lf as a
-    order by Date
-  "
-)
-
-# calculate release needed
-alt_lf <- sqldf(
-  "
-    select a.*,
-      demand_mgd - wd_curr_mgd as need_curr_mgd,
-      demand_mgd - wd_p20_mgd as need_p20_mgd,
-      demand_mgd - wd_q500_mgd as need_q500_mgd,
-      Flow - wd_curr_mgd * 1.547 as Flow_curr,
-      Flow - wd_p20_mgd * 1.547 as Flow_p20,
-      Flow - wd_q500_mgd * 1.547 as Flow_q500
-    from alt_lf as a
-    order by Date
-  "
-)
-
-quantile(alt_lf$need_curr_mgd, probs=c(0.5,0.75,0.8,0.9,0.95,0.99,1.0))
-quantile(alt_lf$need_q500_mgd, probs=c(0.5,0.75,0.8,0.9,0.95,0.99,1.0))
-quantile(alt_lf$need_p20_mgd, probs=c(0.5,0.75,0.8,0.9,0.95,0.99,1.0))
+names(icprb_monthly_lf) <- c('year', 'month', 'Flow')
+icprb_monthly_lf$Date <- as.Date(paste(icprb_monthly_lf$year, icprb_monthly_lf$month,'01',sep='-' ))
+nat_lf <- icprb_monthly_lf
