5c79ef45a084734d24aa85a0842439ae266e1e38
Burgholzer <robert.burgholzer@deq.virginia.gov>
2022-08-01 12:29:37 -0400

    fix paths to go to online github

--- R/permitting/rivanna/vwp_lake_monticello.R
+++ R/permitting/rivanna/vwp_lake_monticello.R
@@ -13,7 +13,8 @@ relid <- 337730
 welid <- 340400 # withdrawal container
 roelid <- 352048 
 
-datf4 <- om_get_rundata(felid, 401, site=omsite)
+datf4 <- om_get_rundata(felid, 400, site=omsite)
+datf6 <- om_get_rundata(felid, 601, site=omsite)
 
 datr4 <- om_get_rundata(relid, 401, site=omsite)
 
@@ -21,3 +22,5 @@ datw4 <- om_get_rundata(welid, 401, site=omsite)
 
 datro4 <- om_get_rundata(roelid, 401, site=omsite)
 
+quantile(datf4$available_mgd)
+quantile(datf6$available_mgd)
