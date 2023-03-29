d8ebd70046c60f64526f5d3979b66d43d29dbb76
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2023-03-29 14:59:54 -0400

    stashing salem scripts

--- R/permitting/Salem WTP/availability.R
+++ R/permitting/Salem WTP/availability.R
@@ -1,5 +1,6 @@
 library("knitr")
 library("kableExtra")
+source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/R/fac_utils.R") #Used until fac_utils is packaged
 library()
 
 basepath='/var/www/R';
@@ -9,20 +10,25 @@ source(paste(basepath,'config.R',sep='/'))
 ds = RomDataSource$new(site, rest_uname)
 ds$get_token(rest_pw)
 
-ccelid = 328319
-cc_dat <- om_get_rundata(ccelid , 222, site = omsite)
-cc_dat$qcu <- ( cc_dat$impoundment_Qin - cc_dat$impoundment_Qout )
-wd_r4 = om_flow_table(cc_dat, 'qcu')
-om_flow_table(cc_dat, 'avail_catawba')
-om_flow_table(cc_dat, 'qcu')
-atc <- om_flow_table(cc_dat, 'avail_tinker')
-kable(atc)
+# Tinker Intake
+elid_tinker = 328319
+dat_tinker_222 <- om_get_rundata(elid_tinker , 222, site = omsite)
+dat_tinker_222av <- om_flow_table(dat_tinker_222, 'avail_tinker')
+kable(dat_tinker_222av, 'markdown')
 
-selid = 306768
-s_dat <- om_get_rundata(selid , 222, site = omsite)
-sav <- om_flow_table(cc_dat, 'available_mgd')
+# Salem WTP (Roanoke River Intake)
+elid_salem = 306768
+# dat_salem_616 <- om_get_rundata(elid_salem , 616, site = omsite)
+# dat_salem_616av <- om_flow_table(dat_salem_616, 'available_mgd')
+dat_salem_222 <- om_get_rundata(elid_salem , 222, site = omsite)
+dat_salem_222av <- om_flow_table(dat_salem_222, 'available_mgd')
+# kable(dat_salem_222av)
+kable(dat_salem_222av, 'markdown')
+
+# Spring Hollow (Roanoke River Intake)
+elid_SH = 328321
+dat_SH_222 <- om_get_rundata(elid_SH , 222, site = omsite)
+dat_SH_222av <- om_flow_table(dat_SH_222, 'available_mgd')
+kable(dat_SH_222av, 'markdown')
 
 
-shelid = 328321
-sh_dat <- om_get_rundata(shelid , 222, site = omsite)
-shav <- om_flow_table(sh_dat, 'available_mgd')
