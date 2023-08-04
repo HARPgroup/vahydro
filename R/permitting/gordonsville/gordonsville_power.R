54c32c9be26ccb88d850253228a23f4e8538e0f1
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2023-08-04 14:59:35 -0400

    gordonsville imp stats and imp geom plot

--- R/permitting/gordonsville/gordonsville_power.R
+++ R/permitting/gordonsville/gordonsville_power.R
@@ -3,7 +3,7 @@ library('zoo')
 library('knitr') # needed for kable()
 basepath='/var/www/R';
 source("/var/www/R/config.R")
-# source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/R/fac_utils.R")
+source("https://raw.githubusercontent.com/HARPgroup/hydro-tools/master/R/fac_utils.R")
 
 
 ################################################################################################
@@ -16,6 +16,20 @@ wb = nhdplusTools::get_waterbodies(id = nhdplus_id)
 wb_wkt = wellknown::sf_convert(wb$geometry)
 plot(wb$geometry)
 wb$areasqkm
+
+# assuming the reservoir is circular in shape
+# surface area max = 12 acres = 522720 sqft
+sa_max = 522720
+r_max = sqrt(sa_max/pi)
+
+# surface area mid = 8 acres = 348480 sqft
+sa_mid = 348480
+r_mid = sqrt(sa_mid/pi)
+
+# imp_geom = data.frame(x=c(0,2,6,10,12),y=c(7,4,0,4,7))
+imp_geom = data.frame(x=c(0,r_max-r_mid,r_max,r_max+r_mid,r_max+r_max),y=c(7,4,0,4,7))
+plot(imp_geom,xlab="ft",ylab="ft")
+lines(imp_geom, type = "l", lty = 1)
 
 ################################################################################################
 ################################################################################################
@@ -54,35 +68,53 @@ wb$areasqkm
 #               ")
 # 
 # 
-# ################################################################################################
-# # LOAD MODEL IDs:
-# rseg_om_id <- 252117 # Smith Mountain and Leesville Dams
-# fac_om_id <- 351208  # SML SERVICE AREA:Smith Mountain and Leesville Dams
-# runid <- 401
-# ################################################################################################
-# 
-# facdat <- om_get_rundata(fac_om_id, runid, site = omsite)
-# rsegdat <- om_get_rundata(rseg_om_id, runid, site = omsite)
-# mstart <- zoo::as.Date(as.POSIXct(min(index(rsegdat)),origin="1970-01-01"))
-# mend <- zoo::as.Date(as.POSIXct(max(index(rsegdat)),origin="1970-01-01"))
-# 
-# facdat_df <- data.frame(facdat)
-# rsegdat_df <- data.frame(rsegdat)
-# 
-# 
-# sort(colnames(rsegdat_df))
-# sort(colnames(facdat_df))
-# 
-# facdat_df$impoundment_use_remain_mg
-# facdat_df$lake_elev
-# #-------------------------------------------------------------------------
-# 
-# SML <- om_quantile_table(facdat_df, metrics = c("historic_monthly_pct","vwp_max_mgy","vwp_max_mgd",
-#                                                 "vwp_base_mgd","wd_mgd","unmet_demand_mgd",
-#                                                 "impoundment_use_remain_mg","lake_elev"),
-#                          rdigits = 3)
-# kable(SML,'markdown')
-# 
-# 
-# ################################################################################################
-# ################################################################################################
+################################################################################################
+# LOAD MODEL IDs:
+rseg_om_id <- 207771 # South Anna River
+fac_om_id <- 284961  # Gordonsville Power Station:South Anna River
+runid <- 401
+################################################################################################
+
+facdat <- om_get_rundata(fac_om_id, runid, site = omsite)
+rsegdat <- om_get_rundata(rseg_om_id, runid, site = omsite)
+mstart <- zoo::as.Date(as.POSIXct(min(index(rsegdat)),origin="1970-01-01"))
+mend <- zoo::as.Date(as.POSIXct(max(index(rsegdat)),origin="1970-01-01"))
+
+facdat_df <- data.frame(facdat)
+rsegdat_df <- data.frame(rsegdat)
+
+sort(colnames(rsegdat_df))
+sort(colnames(facdat_df))
+
+#-------------------------------------------------------------------------
+
+gord <- om_quantile_table(facdat_df, metrics = c("vwp_max_mgy","vwp_max_mgd","wd_mgd",
+                                               "unmet_demand_mgd",
+                                               "local_impoundment_area","local_impoundment_days_remaining", 
+                                               "local_impoundment_demand","local_impoundment_demand_met_mgd", 
+                                               "local_impoundment_evap_mgd",            
+                                               "local_impoundment_lake_elev","local_impoundment_max_usable",     
+                                               "local_impoundment_precip_mgd","local_impoundment_Qin",            
+                                               "local_impoundment_Qout","local_impoundment_refill",         
+                                               "local_impoundment_refill_full_mgd","local_impoundment_release",        
+                                               "local_impoundment_spill",        
+                                               "local_impoundment_Storage","local_impoundment_use_remain_mg"),
+                         rdigits = 3)
+
+
+kable(gord)
+
+################################################################################################
+################################################################################################
+
+# number of years in simulation: 
+nyears = nrow(facdat_df)/365
+
+# wd_mgd should not exceed the Maximum Annual Withdrawal Limit 13.43 mgy
+sum(facdat_df$wd_mgd)/nyears
+
+
+
+
+################################################################################################
+################################################################################################
