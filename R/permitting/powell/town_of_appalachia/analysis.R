2d78b83f4976670bbd6aa49b18d2b5c64d08aa07
Your Name <you@example.com>
2023-12-06 10:50:37 -0500

    fixed hydro inclusion code

--- R/permitting/powell/town_of_appalachia/analysis.R
+++ R/permitting/powell/town_of_appalachia/analysis.R
@@ -3,15 +3,48 @@ source(paste(basepath,'config.R',sep='/'))
 library('knitr')
 library("hydrotools")
 
-elid = 353101 #bedford hydro snowden
-relid = 211633 # James River
-celid = 211669 # channel element in james model
+library(nhdplusTools)
+#> USGS Support Package: https://owi.usgs.gov/R/packages.html#support
+library(sf)
+library("sqldf")
+library("stringr")
+library("rjson")
+
+plat_bb = 36.901388888889; plon_bb = -82.754166666667 # Ben's Branch downstream of impoundment
+plat_imp = 36.9022222222; plon_imp = -82.7525 # Impoundment intake
+plat = 36.90784; plon = -82.76888 # Powell River intake
+
+# Entuire drainage above Powell confluence
+out_point_bb = sf::st_sfc(sf::st_point(c(plon_bb, plat_bb)), crs = 4326)
+nhd_out_bb <- get_nhdplus(out_point_bb)
+dasqmi_bb <- 0.386102 * nhd_out_bb$totdasqkm
+dasqmi_bb
+map_bb <- plot_nhdplus((list(nhd_out_bb$comid)))
+# DA Above dam
+out_point_imp = sf::st_sfc(sf::st_point(c(plon_imp, plat_imp)), crs = 4326)
+nhd_out_imp <- get_nhdplus(out_point_imp)
+dasqmi_imp <- 0.386102 * nhd_out_imp$totdasqkm
+dasqmi_imp
+map_imp <- plot_nhdplus((list(nhd_out_imp$comid)))
+dasqmi_imp / dasqmi_bb
+
+bb_all <- get_nhdplus(map_bb$basin)
+map_bb <- plot_nhdplus((list(nhd_out_bb$comid)))
+plot_nhdplus(list(map_bb$flowline$COMID))
+wb = nhdplusTools::get_waterbodies(id = nhdplus_id)
+wb_wkt = wellknown::sf_convert(wb$geometry)
+plot(wb$geometry)
+bb_network <- get_UT(nhd_out_bb, nhd_out_bb$comid, distance = NULL)
+
+elid = 353101 # Town of A reservoir
+relid = 247367 # Powell River
 runid=400
 gage_number = '01667500' # Rapidan
 startdate <- "1984-10-01"
 enddate <- "2020-09-30"
 pstartdate <- "2008-04-01"
 penddate <- "2008-11-30"
+
 
 rmarkdown::render(
   '/usr/local/home/git/vahydro/R/OWS_summaries/model_run_brief.Rmd', 
@@ -74,22 +107,4 @@ kable(m_stats,'markdown')
 deets <- as.data.frame(mdata[,c(
   "year", "month", "day", "Qin", "Qout", "target", "flowby", "flowby_cov", "min_release", "release"
 )])
-
-
-cdata <- om_get_rundata(celid, runid, site=omsite)
-c_stats <- om_quantile_table(
-  cdata, 
-  metrics = c(
-    "Qin", "Qup", "Qout"
-  ),
-  quantiles=c(0,0.01,0.05,0.1,0.25, 0.5, 0.75, 1.0),
-  rdigits = 2
-)
-kable(c_stats,'markdown')
-
-lfcdata <- cdata[which(cdata$Qout < 1000),]
-plot(100.0*(lfcdata$Storage / lfcdata$depth)/max(cdata$Storage / cdata$depth) ~ lfcdata$Qout, ylim=c(0,100))
-plot(100.0*(cdata$Storage / cdata$depth)/max(cdata$Storage / cdata$depth) ~ cdata$Qout, ylim=c(0,100))
-plot(100.0*(cdata$Storage / cdata$depth)/max(cdata$Storage / cdata$depth) ~ cdata$Qout, ylim=c(0,100), xlim=c(0,1000))
-plot(100.0*(cdata$Storage / cdata$depth)/max(cdata$Storage / cdata$depth) ~ cdata$Qout, ylim=c(0,100), xlim=c(0,2000))
 
